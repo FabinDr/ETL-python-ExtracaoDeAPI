2bf54cf947371253c62a88efea6bbdf8b37abc94
Fabio Duarte <fbduarte.ic@gmail.com>
2025-09-22 15:33:33 -0300

    add file para database

---
+++ src/database.py
@@ -0,0 +1,7 @@
+# Criando em forma de objeto
+#transforma obejeto para sql
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Float, String, Integer, DateTime
+from datetime import datetime
+
+
