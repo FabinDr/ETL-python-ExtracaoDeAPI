2fd428fea24f3d2ca1c4a077cc2f74f91d68435e
Fabio Duarte <fbduarte.ic@gmail.com>
2025-09-29 14:00:59 -0300

    Adciona o dashboard no streamlit

--- app/dashboard.py
+++ app/dashboard.py
@@ -14,6 +14,8 @@ POSTGRES_HOST = os.getenv("POSTGRES_HOST")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
 
+@st.cache_data(ttl=60) # Para deixar o dash mais rápido
+
 def ler_dados():
     try:
         conn = psycopg2.connect(
@@ -27,6 +29,7 @@ def ler_dados():
         query = 'SELECT * FROM bitcoin_precos ORDER BY timestamp DESC'
         df = pd.read_sql(query, conn)
         conn.close()
+        
         return df
     except Exception as e:
         st.error(f'Erro ao conectar com o Banco: {e}')
@@ -34,28 +37,35 @@ def ler_dados():
 
 def main():
     st.set_page_config(page_title="Dashboard de Preços do Bitcoin", layout="wide")
-    st.title("📊 Dashboard de Preços do Bitcoin")
+    st.title("Dashboard de Preços do Bitcoin")
     st.write("Este dashboard exibe os dados do preço do Bitcoin coletados periodicamente em um banco PostgreSQL.")
 
     df = ler_dados()
 
     if not df.empty:
-        st.subheader("📋 Dados Recentes")
-        st.dataframe(df)
+        #Kpi´s
+        st.subheader("🔢 Estatísticas Gerais")
+        col1, col2, col3 = st.columns(3)
+        col1.metric("Preço Atual", f"${df['valor'].iloc[-1]:,.2f}")
+        col2.metric("Preço Máximo (registrado)", f"${df['valor'].max():,.2f}")
+        col3.metric("Preço Mínimo (registrado)", f"${df['valor'].min():,.2f}")
 
         df['timestamp'] = pd.to_datetime(df['timestamp'])
         df = df.sort_values(by='timestamp')
         
         st.subheader("📈 Evolução do Preço do Bitcoin")
         st.line_chart(data=df, x='timestamp', y='valor', use_container_width=True)
-
-        st.subheader("🔢 Estatísticas Gerais")
-        col1, col2, col3 = st.columns(3)
-        col1.metric("Preço Atual", f"${df['valor'].iloc[-1]:,.2f}")
-        col2.metric("Preço Máximo", f"${df['valor'].max():,.2f}")
-        col3.metric("Preço Mínimo", f"${df['valor'].min():,.2f}")
     else:
         st.warning("Nenhum dado encontrado no banco de dados PostgreSQL.")
 
+        st.subheader("Dados Detalhados")
+
+    with st.expander("📋 Tabela com os Dados Recentes"):
+        default_cols = list(df)
+        # Limita a exibição para não travar
+        st.dataframe(df)
+        st.caption(f"Mostrando até 1000 de {len(df)} registros.")
+
 if __name__ == "__main__":
     main()
+
